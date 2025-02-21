88d8fc8dabc883181d83ca32c9e223b581745eda
Doomturtles <jonny.collins111@gmail.com>
2025-02-21 18:56:20 +0000

    Created empty combat state

---
+++ UnityFolder/Through the Thicket/Assets/Scripts/Game/Combat/CombatState.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatState : MonoBehaviour, IGameState
+{
+    public void Initialize(GameManager manager)
+    {
+
+    }
+    public void Open()
+    {
+
+    }
+    public void Close()
+    {
+
+    }
+}

23fcc50d843f34959c3178fd80f88956c4edcc7b
Doomturtles <jonny.collins111@gmail.com>
2025-03-09 14:40:02 +0000

    fleshed out the cellular automata generator slightly

--- UnityFolder/Through the Thicket/Assets/Scripts/Cellular Automata/CellularAutomataGenerator.cs
+++ UnityFolder/Through the Thicket/Assets/Scripts/Cellular Automata/CellularAutomataGenerator.cs
@@ -6,6 +6,32 @@ public static class CellularAutomataGenerator
 {
     public static void GenerateChunkAt(int chunkX, int chunkY, string filePath, int seed)
     {
+        //we need a grid of 9 chunks so we can simulate all surrounding tile generation
+        AutomataChunk[,] chunks = new AutomataChunk[3, 3];
+        GenerateChunks(chunks);
+    }
+    private static void GenerateChunks(AutomataChunk[,] chunks)
+    {
 
+    }
+    enum AutomataTileType
+    {
+        None,
+        Grass,
+        Path,
+        Mud,
+        River
+    }
+    private class AutomataTile
+    {
+        public AutomataTileType type;
+    }
+    private class AutomataChunk
+    {
+        private AutomataTile[,] tiles;
+        private AutomataChunk()
+        {
+            tiles = new AutomataTile[Chunk.ChunkSize(), Chunk.ChunkSize()];
+        }
     }
 }

c7a030380dacaa2138326eabff22580b3045f723
Doomturtles <jonny.collins111@gmail.com>
2025-03-13 19:31:27 +0000

    created initial noise map of river and mud tiles

--- UnityFolder/Through the Thicket/Assets/Scripts/Cellular Automata/CellularAutomataGenerator.cs
+++ UnityFolder/Through the Thicket/Assets/Scripts/Cellular Automata/CellularAutomataGenerator.cs
@@ -6,13 +6,36 @@ public static class CellularAutomataGenerator
 {
     public static void GenerateChunkAt(int chunkX, int chunkY, string filePath, int seed)
     {
-        //we need a grid of 9 chunks so we can simulate all surrounding tile generation
-        AutomataChunk[,] chunks = new AutomataChunk[3, 3];
-        GenerateChunks(chunks);
+        AutomataChunk generatedChunk = GenerateChunk(chunkX, chunkY, seed);
+        //TODO save chunk as at filepath
     }
-    private static void GenerateChunks(AutomataChunk[,] chunks)
+    private static AutomataChunk GenerateChunk(int chunkX, int chunkY, int seed)
     {
-
+        AutomataChunk[,] chunks = InitialNoiseMap(chunkX, chunkY, seed);
+        int iterations = 5;
+        for(int i = 0; i < iterations; i++)
+        {
+            chunks = CellularIteration(chunks);
+        }
+        return chunks[1,1];
+    }
+    private static AutomataChunk[,] InitialNoiseMap(int chunkX, int chunkY, int seed)
+    {
+        //we need a grid of 9 chunks so we can simulate all surrounding tile generation
+        AutomataChunk[,] noiseChunks = new AutomataChunk[3, 3];
+        for(int x = 0; x < 3; x++)
+        {
+            for(int y = 0; y < 3; y++)
+            {
+                noiseChunks[x, y] = new AutomataChunk(chunkX, chunkY, seed);
+            }
+        }
+        return noiseChunks;
+    }
+    private static AutomataChunk[,] CellularIteration(AutomataChunk[,] chunks)
+    {
+        //TODO implement cellular automata rule application
+        return null;
     }
     enum AutomataTileType
     {
@@ -25,13 +48,32 @@ public static class CellularAutomataGenerator
     private class AutomataTile
     {
         public AutomataTileType type;
+        public AutomataTile(AutomataTileType startType)
+        {
+            type = startType;
+        }
     }
     private class AutomataChunk
     {
         private AutomataTile[,] tiles;
-        private AutomataChunk()
+        public AutomataChunk(int chunkX, int chunkY, int seed)
         {
+            //reset the seed and multiply by the chunkX and chunkY so each chunk will have a unique seed
+            UnityEngine.Random.InitState(seed * chunkX * chunkY);
+            for (int x  = 0; x < 16;x++)
+            {
+                for(int y = 0; y < 16; y++)
+                {
+                    tiles[x, y] = new AutomataTile(Random.Range(0, 1) == 1 ? AutomataTileType.River : AutomataTileType.Mud);
+                }
+            }
             tiles = new AutomataTile[Chunk.ChunkSize(), Chunk.ChunkSize()];
         }
     }
+    private abstract class AutomataRule
+    {
+        //Moores neighbourhood is assumed
+        public abstract AutomataTile[,] ApplyRule(AutomataTile[,] tiles);
+    }
+    public 
 }

b6f0544e2fdeb34962c97a17bcb157a1b02de83a
Doomturtles <jonny.collins111@gmail.com>
2025-03-24 11:21:20 +0000

    added small hops to walking animation

--- UnityFolder/Through the Thicket/Assets/Scripts/Game/Player/PlayerAnimator.cs
+++ UnityFolder/Through the Thicket/Assets/Scripts/Game/Player/PlayerAnimator.cs
@@ -25,6 +25,9 @@ public class PlayerAnimator : MonoBehaviour
             currentTime += Time.deltaTime;
             progress = currentTime / duration;
             movementTransform.localPosition = Vector3.Lerp(startPos, endPos, progress);
+
+            float height = Mathf.Abs(Mathf.Sin(progress * 0.5f * Mathf.PI * 2)) * 0.6f;
+            movementTransform.localPosition = new Vector3(movementTransform.localPosition.x, height, movementTransform.localPosition.z);
             yield return null;
         }
         movementTransform.localPosition = endPos;

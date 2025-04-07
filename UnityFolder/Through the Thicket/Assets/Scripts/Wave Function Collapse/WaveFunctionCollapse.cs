26205af724e4e173a03e4eaafb775fa4d8a69d38
Doomturtles <jonny.collins111@gmail.com>
2025-04-07 15:23:12 +0100

    added empty wave function collapse classes

--- UnityFolder/Through the Thicket/Assets/Scripts/Wave Function Collapse/WaveFunctionCollapse.cs
+++ UnityFolder/Through the Thicket/Assets/Scripts/Wave Function Collapse/WaveFunctionCollapse.cs
@@ -1,4 +1,5 @@
 using JetBrains.Annotations;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,5 +9,30 @@ public class WaveFunctionCollapse : MonoBehaviour
     public static void GenerateDungeon(int ID)
     {
         
+    }
+    private enum WFCTileType
+    {
+        None,
+    }
+    private class CollapsedTile
+    {
+        public WFCTileType type;
+        public CollapsedTile(WFCTileType t)
+        {
+            type = t;
+        }
+    }
+    private class WFCTile
+    {
+        public int x;
+        public int y;
+        private List<CollapsedTile> collapsePossibilities;
+        private WFCTile()
+        {
+            foreach(WFCTileType type in Enum.GetValues(typeof(WFCTileType)))
+            {
+                collapsePossibilities.Add(new CollapsedTile(type));
+            }
+        }
     }
 }
